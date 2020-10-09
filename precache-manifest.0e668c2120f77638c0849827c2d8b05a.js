2d37df5662f1a629a01eb9ba9f79267779c05b00
MehediKhokon <mahedikhokon1@gmail.com>
2020-10-09 17:52:59 +0600

    Updates

---
+++ precache-manifest.0e668c2120f77638c0849827c2d8b05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343fa8a186389dad6a96f80a72d502e3",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "355e3e5bb19516e7cba1",
+    "url": "/expense-tracker/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355e3e5bb19516e7cba1",
+    "url": "/expense-tracker/static/js/main.ba27336a.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
